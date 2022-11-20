382c8bbc83a3150c1ce2235b769bf3b6d9df8aa9
muyi2905 <obaremimuyiwa@gmail.com>
2022-11-20 21:51:22 +0000

    final stages

--- index.js
+++ index.js
@@ -21,6 +21,9 @@ image2.setAttribute("src", randomImageSource2);
 if (randomNumber1 > randomNumber2) {
     document.querySelector("h1").innerHTML = "Player 1 wins"
 }
-else {
+else if (randomNumber2 > randomNumber1) {
     document.querySelector("h1").innerHTML = "Player 2 wins"
+}
+else {
+    document.querySelector("h1").innerHTML = "Draw!";
 }
