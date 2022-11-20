5d24d5eb1df35b3e7d4585206ede4c7c6b7905f9
muyi2905 <obaremimuyiwa@gmail.com>
2022-11-20 21:44:17 +0000

    changes

--- index.js
+++ index.js
@@ -8,3 +8,12 @@ let image1 = document.querySelectorAll("img")[0];
 
 image1.setAttribute("src", randomImageSource);
 
+let randomNumber2 = Math.floor(Math.random() * 6 + 1)
+
+let randomDiceImage2 = "dice" + randomNumber2 + ".png"
+
+let randomImageSource2 = "images/" + randomDiceImage;
+
+let image2 = document.querySelectorAll("img")[1];
+
+image1.setAttribute("src", randomImageSource2);
