71de8b542180e5edce231ea081aa0a61a4781526
muyi2905 <obaremimuyiwa@gmail.com>
2022-11-20 21:38:55 +0000

    edit

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
 let randomNumber1 = Math.random() * 6 + 1
-let randomNumber2 = Math.random() * 6 + 1
-
-let image1 = "images/dice6" + randomNumber1 + ".png";
-
-let image2 = "images/dice6" + randomNumber2 + ".png";
-
-document.querySelector(".img1").setAttribute("src", image1);
-
-document.querySelector(".img2").setAttribute("src", image2);
