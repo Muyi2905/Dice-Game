cb33bbdda4b7be983dd352cdcbf0c4a7182e0582
muyi2905 <obaremimuyiwa@gmail.com>
2022-11-20 21:49:04 +0000

    testing

--- index.js
+++ index.js
@@ -16,4 +16,11 @@ let randomImageSource2 = "images/" + randomDiceImage;
 
 let image2 = document.querySelectorAll("img")[1];
 
-image1.setAttribute("src", randomImageSource2);
+image2.setAttribute("src", randomImageSource2);
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 1 wins"
+}
+else {
+    document.querySelector("h1").innerHTML = "Player 2 wins"
+}
