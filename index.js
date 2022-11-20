8afb4e6a7675ad3e63e9d84aed7a7d9cf7a50fe3
muyi2905 <obaremimuyiwa@gmail.com>
2022-11-20 21:59:40 +0000

    done

--- index.js
+++ index.js
@@ -1,28 +1,58 @@
-let randomNumber1 = Math.floor(Math.random() * 6 + 1)
+// let randomNumber1 = Math.floor(Math.random() * 6 + 1)
 
-let randomDiceImage = "dice" + randomNumber1 + ".png"
+// let randomDiceImage = "dice" + randomNumber1 + ".png"
 
-let randomImageSource = "images/" + randomDiceImage;
+// let randomImageSource = "images/" + randomDiceImage;
 
-let image1 = document.querySelectorAll("img")[0];
+// let image1 = document.querySelectorAll("img")[0];
+
+// image1.setAttribute("src", randomImageSource);
+
+// let randomNumber2 = Math.floor(Math.random() * 6 + 1)
+
+// let randomDiceImage2 = "dice" + randomNumber2 + ".png"
+
+// let randomImageSource2 = "images/" + randomDiceImage;
+
+// let image2 = document.querySelectorAll("img")[1];
+
+// image2.setAttribute("src", randomImageSource2);
+
+// if (randomNumber1 > randomNumber2) {
+//     document.querySelector("h1").innerHTML = "Player 1 wins"
+// }
+// else if (randomNumber2 > randomNumber1) {
+//     document.querySelector("h1").innerHTML = "Player 2 wins"
+// }
+// else {
+//     document.querySelector("h1").innerHTML = "Draw!";
+// }
+
+
+var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
+
+var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
+
+var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice6.png
+
+var image1 = document.querySelectorAll("img")[0];
 
 image1.setAttribute("src", randomImageSource);
 
-let randomNumber2 = Math.floor(Math.random() * 6 + 1)
 
-let randomDiceImage2 = "dice" + randomNumber2 + ".png"
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-let randomImageSource2 = "images/" + randomDiceImage;
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
 
-let image2 = document.querySelectorAll("img")[1];
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
-image2.setAttribute("src", randomImageSource2);
 
+//If player 1 wins
 if (randomNumber1 > randomNumber2) {
-    document.querySelector("h1").innerHTML = "Player 1 wins"
+    document.querySelector("h1").innerHTML = "🚩 Play 1 Wins!";
 }
 else if (randomNumber2 > randomNumber1) {
-    document.querySelector("h1").innerHTML = "Player 2 wins"
+    document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
 }
 else {
     document.querySelector("h1").innerHTML = "Draw!";
